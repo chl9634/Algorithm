cfa4f4140ec658f7bd6ea11b4312bc687dab224e
chl9634 <54919526+chl9634@users.noreply.github.com>
2025-08-05 16:10:48 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int N, M;
+    cin >> N >> M;
+    
+    if(N < 8 || N > 50 || M < 8 || M > 50)
+        return 0;
+
+    char board[50][50];
+    for (int i = 0; i < N; i++) 
+    {
+        for (int j = 0; j < M; j++)
+            cin >> board[i][j];
+    }
+
+    int minCount = 64;
+
+    for (int i = 0; i <= N - 8; i++) 
+    {
+        for (int j = 0; j <= M - 8; j++) 
+        {
+            int p1 = 0, p2 = 0;
+            
+            for (int p = 0; p < 8; p++) 
+            {
+                for (int q = 0; q < 8; q++) 
+                {
+                    char current = board[i + p][j + q];
+                    if ((p + q) % 2 == 0) 
+                    {
+                        if (current != 'W') 
+                            p1++;
+                        if (current != 'B') 
+                            p2++;
+                    } else 
+                    {
+                        if (current != 'B') 
+                            p1++;
+                        if (current != 'W') 
+                            p2++;
+                    }
+                }
+            }
+
+            int minimum = min(p1, p2);
+            if (minimum < minCount) minCount = minimum;
+        }
+    }
+    cout << minCount << '\n';
+    return 0;
+}
