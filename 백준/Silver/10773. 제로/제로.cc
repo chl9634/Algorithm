818d6fc147991f1c2b59e2b5a17651aba8165da8
chl9634 <54919526+chl9634@users.noreply.github.com>
2025-09-18 22:06:29 +0900

    [Silver IV] Title: 제로, Time: 4 ms, Memory: 2548 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+stack<int> stack_zero;
+
+// 입력
+void inputFunc(int K) 
+{
+    for (int i = 0; i < K; i++) 
+    {
+        int num;
+        cin >> num;
+        if (num == 0) 
+        {
+            if (!stack_zero.empty()) 
+                stack_zero.pop();
+        } 
+        else
+            stack_zero.push(num);
+    }
+}
+
+// 합 계산
+long long sum() 
+{
+    long long sum = 0;
+    while (!stack_zero.empty()) 
+    {
+        sum += stack_zero.top();
+        stack_zero.pop();
+    }
+    return sum;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int K;
+    cin >> K;
+    
+    if(K < 1 || K > 1000000) return 0;
+
+    inputFunc(K);
+    cout << sum() << "\n";
+
+    return 0;
+}
